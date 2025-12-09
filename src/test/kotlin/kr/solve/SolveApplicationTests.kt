f4da61210c8c45976f716994d6b9b0d9ddcc345d
jbj338033 <jbj338033@gmail.com>
2025-12-10 01:22:15 +0900

    feat: main test

---
+++ src/test/kotlin/kr/solve/SolveApplicationTests.kt
@@ -0,0 +1,11 @@
+package kr.solve
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SolveApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
