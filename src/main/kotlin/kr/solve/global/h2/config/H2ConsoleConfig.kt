fd23e0bc9197f195a655806a35d6d57100b1055e
jbj338033 <jbj338033@gmail.com>
2025-12-10 01:18:11 +0900

    feat: h2 web server config

---
+++ src/main/kotlin/kr/solve/global/h2/config/H2ConsoleConfig.kt
@@ -0,0 +1,13 @@
+package kr.solve.global.h2.config
+
+import org.h2.tools.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("local")
+class H2ConsoleConfig {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    fun h2WebServer(): Server = Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082")
+}
