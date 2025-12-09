29a9c85dfe1da303c8daabc65b3aebc07d44f57f
jbj338033 <jbj338033@gmail.com>
2025-12-10 01:14:34 +0900

    feat: tag presentation

---
+++ src/main/kotlin/kr/solve/domain/tag/presentation/request/CreateTagRequest.kt
@@ -0,0 +1,10 @@
+package kr.solve.domain.tag.presentation.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class CreateTagRequest(
+    @field:NotBlank
+    @field:Size(max = 50)
+    val name: String,
+)
---
+++ src/main/kotlin/kr/solve/domain/tag/presentation/response/TagResponse.kt
@@ -0,0 +1,19 @@
+package kr.solve.domain.tag.presentation.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+import kr.solve.domain.tag.domain.entity.Tag
+import java.util.UUID
+
+fun Tag.toResponse() =
+    TagResponse(
+        id = id,
+        name = name,
+    )
+
+@Schema(name = "Tag", description = "태그 정보")
+data class TagResponse(
+    @Schema(description = "태그 ID")
+    val id: UUID,
+    @Schema(description = "태그 이름", example = "DP")
+    val name: String,
+)
