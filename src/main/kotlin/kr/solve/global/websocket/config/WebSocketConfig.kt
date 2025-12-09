12f0da8e70e42d8a4041f52baddca62955df4f66
jbj338033 <jbj338033@gmail.com>
2025-12-10 01:19:51 +0900

    feat: web socket config

---
+++ src/main/kotlin/kr/solve/global/websocket/config/WebSocketConfig.kt
@@ -0,0 +1,29 @@
+package kr.solve.global.websocket.config
+
+import kr.solve.domain.execution.presentation.websocket.ExecutionWebSocketHandler
+import kr.solve.domain.submission.presentation.websocket.SubmissionWebSocketHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
+
+@Configuration
+class WebSocketConfig {
+    @Bean
+    fun webSocketHandlerMapping(
+        executionWebSocketHandler: ExecutionWebSocketHandler,
+        submissionWebSocketHandler: SubmissionWebSocketHandler,
+    ): HandlerMapping {
+        val map =
+            mapOf(
+                "/ws/executions" to executionWebSocketHandler,
+                "/ws/submissions" to submissionWebSocketHandler,
+            )
+        return SimpleUrlHandlerMapping(map, Ordered.HIGHEST_PRECEDENCE)
+    }
+
+    @Bean
+    fun webSocketHandlerAdapter(): WebSocketHandlerAdapter = WebSocketHandlerAdapter()
+}
