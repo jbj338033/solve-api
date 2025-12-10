0612167ca3dbb36e347599252923cfc2fbc4a281
jbj338033 <jbj338033@gmail.com>
2025-12-10 22:50:38 +0900

    refactor: judge service to use worker client with streaming

---
+++ src/main/kotlin/kr/solve/infra/judge/JudgeEvent.kt
@@ -0,0 +1,30 @@
+package kr.solve.infra.judge
+
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import kr.solve.domain.submission.domain.enums.JudgeResult
+import java.util.UUID
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes(
+    JsonSubTypes.Type(value = JudgeEvent.Progress::class, name = "PROGRESS"),
+    JsonSubTypes.Type(value = JudgeEvent.Complete::class, name = "COMPLETE"),
+)
+sealed class JudgeEvent {
+    data class Progress(
+        val testcaseId: UUID,
+        val result: JudgeResult,
+        val time: Int,
+        val memory: Int,
+        val score: Int,
+        val progress: Int,
+    ) : JudgeEvent()
+
+    data class Complete(
+        val result: JudgeResult,
+        val score: Int,
+        val time: Int,
+        val memory: Int,
+        val error: String?,
+    ) : JudgeEvent()
+}
