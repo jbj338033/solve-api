dbbe9d4a675df0c95fa1b12fc5c81c461335f2ec
jbj338033 <jbj338033@gmail.com>
2025-12-10 22:50:09 +0900

    remove: isolate executor (moved to worker)

--- src/main/kotlin/kr/solve/infra/isolate/CompileResult.kt
+++
@@ -1,8 +0,0 @@
-package kr.solve.infra.isolate
-
-data class CompileResult(
-    val success: Boolean,
-    val error: String?,
-    val boxPath: String?,
-    val boxId: Int?,
-)
--- src/main/kotlin/kr/solve/infra/isolate/IsolateStatus.kt
+++
@@ -1,9 +0,0 @@
-package kr.solve.infra.isolate
-
-enum class IsolateStatus {
-    OK,
-    TIME_LIMIT_EXCEEDED,
-    MEMORY_LIMIT_EXCEEDED,
-    RUNTIME_ERROR,
-    INTERNAL_ERROR,
-}
