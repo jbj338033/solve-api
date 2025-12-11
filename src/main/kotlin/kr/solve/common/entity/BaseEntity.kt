fe573615e47734acba2a019dd4d8039272222cbc
jbj338033 <jbj338033@gmail.com>
2025-12-11 21:27:03 +0900

    refactor: use callback for entity auditing

--- src/main/kotlin/kr/solve/common/entity/BaseEntity.kt
+++ src/main/kotlin/kr/solve/common/entity/BaseEntity.kt
@@ -1,16 +1,12 @@
 package kr.solve.common.entity
 
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.relational.core.mapping.Column
 import java.time.LocalDateTime
 
 abstract class BaseEntity {
-    @CreatedDate
     @Column("created_at")
     var createdAt: LocalDateTime? = null
 
-    @LastModifiedDate
     @Column("updated_at")
     var updatedAt: LocalDateTime? = null
 }
