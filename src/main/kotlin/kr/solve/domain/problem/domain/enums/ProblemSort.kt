a05b3480990c7c5296ab0ff62fcca4726a9dcc96
jbj338033 <jbj338033@gmail.com>
2025-12-12 01:04:35 +0900

    feat: add problem sort enum

---
+++ src/main/kotlin/kr/solve/domain/problem/domain/enums/ProblemSort.kt
@@ -0,0 +1,10 @@
+package kr.solve.domain.problem.domain.enums
+
+enum class ProblemSort {
+    LATEST,
+    DIFFICULTY_ASC,
+    DIFFICULTY_DESC,
+    ACCEPT_RATE_DESC,
+    SUBMISSIONS_DESC,
+    ACCEPTED_USERS_DESC,
+}
