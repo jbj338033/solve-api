d956863ff20fce9a7a43b2316666d28950e125bf
jbj338033 <jbj338033@gmail.com>
2025-12-12 17:23:02 +0900

    refactor: remove redundant migration files

--- src/main/resources/db/migration/V2__rename_type_columns.sql
+++
@@ -1,5 +0,0 @@
--- Rename contestType to type in contests table
-ALTER TABLE contests RENAME COLUMN contest_type TO type;
-
--- Rename problemType to type in problems table
-ALTER TABLE problems RENAME COLUMN problem_type TO type;
