08ee5e2c0314d485a88e47b56abdef24ca2875b0
jbj338033 <jbj338033@gmail.com>
2025-12-12 16:00:27 +0900

    fix: add missing exception handler return

--- src/main/kotlin/kr/solve/common/error/CommonError.kt
+++ src/main/kotlin/kr/solve/common/error/CommonError.kt
@@ -7,6 +7,10 @@ enum class CommonError(
     override val status: HttpStatus,
     override val message: String,
 ) : BaseError {
+    INVALID_REQUEST(
+        HttpStatus.BAD_REQUEST,
+        "잘못된 요청입니다.",
+    ),
     INTERNAL_SERVER_ERROR(
         HttpStatus.INTERNAL_SERVER_ERROR,
         "서버에서 알 수 없는 오류가 발생했습니다.",
--- src/main/kotlin/kr/solve/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/kr/solve/global/error/GlobalExceptionHandler.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.server.ServerWebInputException
 
 private val logger = KotlinLogging.logger {}
 
@@ -16,7 +17,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleMethodArgumentNotValidException(exception: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
-
+        return ErrorResponse.of(CommonError.INVALID_REQUEST)
     }
 
     @ExceptionHandler(Exception::class)
